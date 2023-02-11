d02b527706cbbed9ba458bb80c33a3505fa2f6cb
Fiona Kate <fkmorgan@me.com>
2023-02-11 09:58:30 +0000

    Initial Commit

---
+++ dictio/dictioApp.swift
@@ -0,0 +1,20 @@
+//
+//  dictioApp.swift
+//  dictio
+//
+//  Created by Fiona Kate Morgan on 11/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct dictioApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
